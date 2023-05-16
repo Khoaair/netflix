e79a69d9f3f7d582aa1864ec96e8772b285d5fa1
KhoaAIr <KhoaAir@gmail.com>
2023-05-16 09:36:44 +0700

    complete restful API

--- api/Routes/lists.js
+++ api/Routes/lists.js
@@ -17,4 +17,44 @@ router.post('/', verify, async (req, res) => {
   }
 });
 
+// DELETE
+router.delete('/:id', verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await List.findByIdAndDelete();
+      res.status(201).json('The list has been deleted...!');
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json('You are not allowed!');
+  }
+});
+
+// GET
+router.get('/', verify, async (req, res) => {
+  const typeQuery = req.query.type;
+  const gerneQuery = req.query.gerne;
+  let list = [];
+  try {
+    if (typeQuery) {
+      if (gerneQuery) {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery, gerne: gerneQuery } },
+        ]);
+      } else {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery } },
+        ]);
+      }
+    } else {
+      list = await List.aggregate([{ $sample: { size: 10 } }]);
+    }
+    return res.status(200).json(list);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 module.exports = router;
