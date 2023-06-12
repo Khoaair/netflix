458130d2162e8ce32c885d8c6afc4277b2ac5b3a
KhoaAir win <KhoaAir@gmail.com>
2023-06-12 12:08:23 +0700

    change backend site

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const customFetch = axios.create({
-  baseURL: 'https://mern-netflix-clone-2023.vercel.app/',
+  baseURL: 'https://mern-netflix-clone-2023.vercel.app/api/',
 });
 
 export default customFetch;
