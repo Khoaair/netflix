1d68f2805b1fca8bc4503b61108bb53a3179da3b
KhoaAir win <KhoaAir@gmail.com>
2023-06-30 10:03:21 +0700

    remove

--- src/components/index.jsx
+++
@@ -1,4 +0,0 @@
-import Topbar from './Topbar';
-import Sidebar from './Sidebar';
-
-export { Topbar, Sidebar };
--- src/utils/axios.jsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const customFetch = axios.create({
-  baseURL: 'https://mern-netflix-clone-2023.vercel.app/api/',
-});
-export default customFetch;
